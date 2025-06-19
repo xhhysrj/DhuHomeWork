dbe5e97234d024751b007bb26202cdb1c77397d5
Dick-ing <1683998081@qq.com>
2025-06-19 11:24:44 +0800

    邻接表构建
    111

--- Group_Project/Group_Project/graph.cpp
+++ Group_Project/Group_Project/graph.cpp
@@ -1,2 +1,39 @@
 #pragma once
 #include "graph.h"
+#include<iostream>
+#include<sstream>
+#include<vector>
+#include<string>
+#include<fstream>
+using namespace std;
+Motor_graph::Motor_graph() {
+	//读取站点信息
+	ifstream station_file("Station.csv");
+	string line;
+	Vers = 0;
+	getline(station_file, line);
+	while (getline(station_file, line)) {
+		stringstream SS(line);
+		vector<string>tokens;
+		string token;
+		while (getline(SS, token, ',')) {
+			tokens.push_back(token);
+		}
+		Station.emplace_back(stoi(tokens[0]), tokens[1], tokens[2], tokens[3]);
+		Vers++;
+	}
+	//读取路线信息
+	ifstream edge_file("Station.csv");
+	edges.resize(Vers);
+	string edge_line;
+	getline(edge_file, edge_line);
+	while (getline(edge_file, edge_line)) {
+		stringstream SS(edge_line);
+		vector<string>tokens;
+		string token;
+		while (getline(SS, token, ',')) {
+			tokens.push_back(token);
+		}
+		edges[stoi(tokens[0]) - 1001].emplace_back(stoi(tokens[1]), tokens[2], tokens[4]);
+	}
+}
--- Group_Project/Group_Project/graph.h
+++ Group_Project/Group_Project/graph.h
@@ -1,1 +1,26 @@
 #pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+#include<iostream>
+#include<list>
+#include<vector>
+#include<string>
+#include"station.h"
+using namespace std;
+class edge {
+public:
+	string line;
+	int time;
+	int next_station_id;
+	edge(int next_station_id,string line,int time ):line(line),time(time),next_station_id(next_station_id){}
+};
+class Motor_graph {
+public:
+	int Vers;
+	vector<Station> Station;
+	vector<list<edge>>edges;
+	Motor_garph();//默认构造函数，从文件中读取站点和路线信息
+	void Motor_garph_reset();//用Stations文件重置站点状态
+	void Motor_graph_update();//用update_station_status文件更新站点状态
+};
+#endif // !GRAPH_H
